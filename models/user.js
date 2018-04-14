2da37ec7937eea4ad08a63e79eff7fe382e8d0d8
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-14 16:39:09 -0500

    Created schema for user data

--- models/user.js
+++ models/user.js
@@ -1,2 +1,45 @@
 //We will need mongoose in order to create schema for our database
 const mongoose = require('mongoose');
+//Fruit schema, the model we want our information to be in the DB
+const userSchema = new mongoose.Schema({
+	name:{
+		type: String,
+		required: true
+	},
+	userNAme:{
+		type: String,
+		required: true,
+		unique: true
+	},
+	password:{
+		type: String,
+		required: true,
+		unique: true
+	},
+	age:{
+		type: Number,
+		required: false
+	},
+	gender:{
+		type: String,
+		required: false,
+		default: 'Other'
+	},
+	profilePicture:{
+		type:String,
+		default: 'https://npengage.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png'
+
+	},
+	photo:{
+		type:[String]
+
+	}
+
+
+
+})
+
+
+
+//Export for controller to use model
+module.exports = mongoose.model('User',userSchema);
