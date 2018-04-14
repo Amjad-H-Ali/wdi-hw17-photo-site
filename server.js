392fe36eaa3477ff20ae44339330d2ac0605c1e0
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-13 21:15:08 -0500

    Installed middleware and other dependencies; gitignored node_mods; And Set up basic server

---
+++ server.js
@@ -0,0 +1,46 @@
+//set up expressJS server
+const express = require('express');
+const app = express();
+//Port we will use
+const PORT = 3000;
+
+//Connect/Require the Database
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Set up server to listen on PORT 3000
+app.listen(PORT, ()=>{
+	console.log(`Server is listening on PORT ${PORT}...`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
